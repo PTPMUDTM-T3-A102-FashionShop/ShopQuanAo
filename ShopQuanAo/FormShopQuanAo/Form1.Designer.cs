fc9da6386d23ad491237c80bf4aaf3e83cf4fd00
gialongchuai <gialongchuai@gmail.com>
2024-10-18 20:03:31 +0700

    test commit

--- ShopQuanAo/FormShopQuanAo/Form1.Designer.cs
+++ ShopQuanAo/FormShopQuanAo/Form1.Designer.cs
@@ -28,20 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.SuspendLayout();
-            // 
-            // Form1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "Form1";
-            this.Text = "it";
-            this.ResumeLayout(false);
+			this.button1 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(166, 121);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(130, 65);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "button1";
+			this.button1.UseVisualStyleBackColor = true;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(900, 562);
+			this.Controls.Add(this.button1);
+			this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+			this.Name = "Form1";
+			this.Text = "it";
+			this.ResumeLayout(false);
 
         }
 
-        #endregion
-    }
+		#endregion
+
+		private System.Windows.Forms.Button button1;
+	}
 }
 
