af2a866341fbabf7d5e07b3e9bfe0bfb9586bdbd
DuyCorn <ngohoangduy0103@gmail.com>
2024-12-08 21:16:01 +0700

    xàm xàm

--- ShopQuanAo/DTO/Properties/Settings.Designer.cs
+++ ShopQuanAo/DTO/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace DTO.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.10.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.11.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -25,14 +25,25 @@ namespace DTO.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=MSI\\SUCOBATNGO;Initial Catalog=DoAnKetMon_UDTM;Integrated Security=Tr" +
-            "ue")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=LAPTOP-AU6VJ49A\\SQLEXPRESS;Initial Catalog=DoAnKetMon_UDTM21114;Integ" +
+            "rated Security=True;TrustServerCertificate=True")]
         public string DoAnKetMon_UDTMConnectionString {
             get {
                 return ((string)(this["DoAnKetMon_UDTMConnectionString"]));
             }
             set {
                 this["DoAnKetMon_UDTMConnectionString"] = value;
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=LAPTOP-AU6VJ49A\\SQLEXPRESS;Initial Catalog=DoAnKetMon_UDTM21114;Integ" +
+            "rated Security=True;TrustServerCertificate=True")]
+        public string DoAnKetMon_UDTM21114ConnectionString {
+            get {
+                return ((string)(this["DoAnKetMon_UDTM21114ConnectionString"]));
             }
         }
     }
